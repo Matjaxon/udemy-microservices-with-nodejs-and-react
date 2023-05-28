80068a50f538ae057a5e74f6590263d737ca1b20
Matt Jackson <mattrjackson1@gmail.com>
2023-05-27 18:12:24 -0700

    Built query service. Began publishing events to query service whenever a resource is created. Distributed that event through the query service to the services managing each of the resource types.

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import PostCreate from './PostCreate';
+import PostList from './PostList';
 
-const app = () => <div>Hello</div>;
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
+    </div>
+  );
+};
 
-export default app;
+export default App;
